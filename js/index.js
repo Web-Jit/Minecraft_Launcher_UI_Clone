7f415a06b1f06057e19903b74d891946367d6714
Web Jeet <webjit45@gmail.com>
2022-10-23 18:36:00 +0530

    Added Files

---
+++ js/index.js
@@ -0,0 +1,50 @@
+function javaedition(){
+    document.getElementById('game-bg').style.backgroundImage = 'url(./images/java-edition.jpg)';
+    document.getElementById('gameedition').innerHTML = 'MINECRAFT JAVA EDITION';
+    document.getElementById('play-btn').innerHTML = 'PLAY';
+    document.getElementById('header2').style.display = 'none';
+    document.getElementById('header3').style.display = 'none';
+    document.getElementById('header4').style.display = 'block';
+    document.getElementById('header5').style.display = 'block';
+    document.getElementById('header6').style.display = 'block';
+}
+function bedrockedition(){
+    document.getElementById('game-bg').style.backgroundImage = 'url(./images/m-bg.jpg)';
+    document.getElementById('gameedition').innerHTML = 'MINECRAFT FOR WINDOWS';
+    document.getElementById('play-btn').innerHTML = 'PLAY';
+    document.getElementById('header2').style.display = 'none';
+    document.getElementById('header3').style.display = 'block';
+    document.getElementById('header4').style.display = 'none';
+    document.getElementById('header5').style.display = 'none';
+    document.getElementById('header6').style.display = 'block';
+}
+function minecraftdungeons(){
+    document.getElementById('game-bg').style.backgroundImage = 'url(./images/m-dungeons-bg.jpg)';
+    document.getElementById('gameedition').innerHTML = 'MINECRAFT DEUNGEONS';
+    document.getElementById('play-btn').innerHTML = 'BUY NOW';
+    document.getElementById('header2').style.display = 'block';
+    document.getElementById('header3').style.display = 'block';
+    document.getElementById('header4').style.display = 'block';
+    document.getElementById('header5').style.display = 'none';
+    document.getElementById('header6').style.display = 'block';
+}
+function minecraftlegends(){
+    document.getElementById('game-bg').style.backgroundImage = 'url(./images/m-legends-bg.jpg)';
+    document.getElementById('gameedition').innerHTML = 'MINECRAFT LEGENDS';
+    document.getElementById('play-btn').innerHTML = 'NEW CONTENT';
+    document.getElementById('header2').style.display = 'none';
+    document.getElementById('header3').style.display = 'none';
+    document.getElementById('header4').style.display = 'none';
+    document.getElementById('header5').style.display = 'none';
+    document.getElementById('header6').style.display = 'none';
+}
+function minecraftlive(){
+    document.getElementById('game-bg').style.backgroundImage = 'url(./images/live-bg.jpg)';
+    document.getElementById('gameedition').innerHTML = 'MINECRAFT LIVE';
+    document.getElementById('play-btn').innerHTML = 'WATCH LIVE';
+    document.getElementById('header2').style.display = 'none';
+    document.getElementById('header3').style.display = 'none';
+    document.getElementById('header4').style.display = 'none';
+    document.getElementById('header5').style.display = 'none';
+    document.getElementById('header6').style.display = 'none';
+}
